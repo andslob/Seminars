52367cfc580cdf8d46ad10da3324ee77d6841ba9
Andrey <andslob@inbox.ru>
2022-11-30 10:02:11 +0500

    1222

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -5,7 +5,7 @@ Console.Write("Введите 1-ое число: ");
 int n = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите 2-ое число: ");
 int m = Convert.ToInt32(Console.ReadLine());
-if (m * m == n)
+if (m * m == n) // != (неравно)
     Console.WriteLine("yes");
 else
     Console.WriteLine("no");
