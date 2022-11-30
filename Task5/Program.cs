458dde1f6d5a5f9866030b6f5597f01baacde7c3
Andrey <andslob@inbox.ru>
2022-11-30 10:00:06 +0500

    111

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task1
+Console.Clear();
+
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (m * m == n)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
