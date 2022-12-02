2da4497a5f865df5a8cf68e16d7e4c52dbc3e520
Andrey <andslob@inbox.ru>
2022-12-02 12:45:34 +0500

    4

--- S2_Task4/Program.cs
+++ S2_Task4/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task 12
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n % m == 0)
+    Console.WriteLine("Кратно");
+else
+    Console.WriteLine($"Не кратно, остаток {n % m}");
