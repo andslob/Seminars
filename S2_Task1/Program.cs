c36babe516e25bd27369f5b579ac8d940770a7dc
Andrey <andslob@inbox.ru>
2022-12-02 12:12:52 +0500

    1

--- S2_Task1/Program.cs
+++ S2_Task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+
+int n = new Random().Next(10, 100); // [begin, end + 1] [10, 99]
+Console.WriteLine($"Случаное число {n}");
+int n1 = n / 10; // кол-во десятков
+int n2 = n % 10; // кол-во единиц
+if (n1 > n2)
+    Console.WriteLine(n1);
+else
+    Console.WriteLine(n2);
--- Task5/Program.cs
+++ Task5/Program.cs
@@ -1,13 +1,11 @@
 ﻿// task1
 Console.Clear();
 
-Console.Write("Введите 1-ое число: ");
-int n = Convert.ToInt32(Console.ReadLine());
-Console.Write("Введите 2-ое число: ");
-int m = Convert.ToInt32(Console.ReadLine());
-if (m * m == n) // != (неравно)
-    Console.WriteLine("yes");
-if (n * n == m) // != (неравно)
-    Console.WriteLine("yes");
+Console.Write("Введите a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b > a) 
+    Console.WriteLine("Результат: ", b);
 else
-    Console.WriteLine("no");
+    Console.WriteLine("Результат: ", a);
