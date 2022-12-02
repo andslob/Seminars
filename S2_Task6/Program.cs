9a581ebe108ffc00602cb32318199c6ed906c9c6
Andrey <andslob@inbox.ru>
2022-12-02 13:08:38 +0500

    666

--- S2_Task6/Program.cs
+++ S2_Task6/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// task 16
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n * n == m)
+    Console.WriteLine("yes");
+else if (m * m == n)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
+
+
+
+// task 16
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.R
+// task 16
+
+Console.Clear();
+Console.Write("Введите 1-ое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2-ое число: ");
+int m = Convert.ToInt32(Console.ReadLine());
+if (n * n == m || m * m == n)
+    Console.WriteLine("yes");
+else
+    Console.WriteLine("no");
+
+// для ДЗ
+
+// dop
+
+Console.Clear();
+
+int n = Convert.ToInt32(Console.ReadLine());
+while (n != 0)
+{
+    n = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("end");
+
