52b1892e3249da67fa9e811265d7a5741bd5fe7f
Andrey <andslob@inbox.ru>
2022-12-06 12:54:47 +0500

    111

--- S3_Task4/Program.cs
+++ S3_Task4/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Task 21
+
+Console.Clear();
+Console.Write("Введите X первой точки: ");
+double x1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y первой точки: ");
+double y1 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите X второй точки: ");
+double x2 = Convert.ToDouble(Console.ReadLine());
+Console.Write("Введите Y второй точки: ");
+double y2 = Convert.ToDouble(Console.ReadLine());
+
+double dist = Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+
+Console.WriteLine(Math.Round(dist, 2));
