8028bbe7900e28001ea1860613f4d10153403b79
Andrey <andslob@inbox.ru>
2022-12-13 12:16:33 +0500

    1

--- S5/Task_1/Program.cs
+++ S5/Task_1/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 31 task
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-9, 10); // [-9, 9]
+}
+
+
+void ReleaseArray(int[] array)
+{
+    int sumPositive = 0, sumNegative = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+            sumPositive += array[i];
+        else
+            sumNegative += array[i];
+    }
+    Console.WriteLine($"Сумма положительных чисел равна {sumPositive}");
+    Console.WriteLine($"Сумма отрицательных чисел равна {sumNegative}");
+}
+
+
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[n];
+InputArray(array);
+Console.WriteLine($"[{string.Join(", ", array)}]");
+ReleaseArray(array);
+
