e5a1a6faffaae7bbfcc26b9ab361b3b7b42ce550
Andrey <andslob@inbox.ru>
2022-12-13 12:47:44 +0500

    5

--- S5/Task_4/Program.cs
+++ S5/Task_4/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 35 task
+
+void InputArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+        array[i] = new Random().Next(-100, 101); // [-100, 100]
+}
+
+
+int ReleaseArray(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] >= 10 && array[i] <= 99)
+            count++; // count = count + 1;
+    }
+    return count;
+}
+
+
+Console.Clear();
+int[] array = new int[123];
+InputArray(array);
+Console.WriteLine($"Начальный массив: [{string.Join(", ", array)}]");
+Console.WriteLine(ReleaseArray(array));
