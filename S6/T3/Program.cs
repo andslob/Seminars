40bc77f5e299e11a1cadd8a7af78a2f7da7a2b11
Andrey <andslob@inbox.ru>
2022-12-16 13:09:33 +0500

    3

--- S6/T3/Program.cs
+++ S6/T3/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 42
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+string result = string.Empty;
+while (n > 0)
+{
+    result = Convert.ToString(n % 2) + result;
+    n /= 2; // n = n / 2;
+}
+Console.WriteLine(result);
