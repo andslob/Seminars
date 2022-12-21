74e41435b51ab96a72e047e39d7dab7c7fb9ab1e
Andrey <andslob@inbox.ru>
2022-12-21 16:39:36 +0500

    sdf

---
+++ S4/S4_T7/Program.cs
@@ -0,0 +1,8 @@
+﻿// task 28
+
+Console.Clear();
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 2; i <= n; i++)
+    result = result * i;
+Console.WriteLine(result);
