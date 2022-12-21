2681bea4206d18fa4257800a4303b30b6f4c7440
Andrey <andslob@inbox.ru>
2022-12-21 16:26:16 +0500

    qwe

---
+++ S1/Task5/Program.cs
@@ -0,0 +1,11 @@
+﻿// task1
+Console.Clear();
+
+Console.Write("Введите a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (b > a) 
+    Console.WriteLine("Результат: ", b);
+else
+    Console.WriteLine("Результат: ", a);
