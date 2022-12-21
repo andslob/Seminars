042bd22b045691a4ecafbb5019acf7d2bc224991
Andrey <andslob@inbox.ru>
2022-12-21 16:24:35 +0500

    kjh

---
+++ S1/Task2/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n % 400 == 0 || n % 100 != 0 && n % 4 == 0) // ; не ставить 
+    Console.WriteLine("yes");
+else 
+{
+    Console.WriteLine("no");
+}
