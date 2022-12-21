64f977155f6ea5afa66849a63d305afec7d25b5a
Andrey <andslob@inbox.ru>
2022-12-21 16:37:53 +0500

    asfd

---
+++ S4/S4_T2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Array
+// Array
+// Double numbers
+Console.Clear();
+Console.Write("Введите кол-во элементов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[n];
+for (int i = 0; i < array.Length; i++)
+{
+    //                                              * (end - begin) + begin
+    array[i] = Math.Round(new Random().NextDouble() * (50 - 10) + 10, 2); // [10, 50]
+}
+
+Console.WriteLine($"[{string.Join(", ", array)}]");
