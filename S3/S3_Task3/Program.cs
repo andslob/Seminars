cfd6b1dfe70530efd127a3b048408fc1e0a64aad
Andrey <andslob@inbox.ru>
2022-12-21 16:34:36 +0500

    wqe

---
+++ S3/S3_Task3/Program.cs
@@ -0,0 +1,14 @@
+﻿// Task 17
+
+Console.Clear();
+Console.Write("Введите четверть: ");
+int n = Convert.ToInt32(Console.ReadLine());
+if (n == 1 || n == 2)
+    Console.WriteLine("y > 0");
+else
+    Console.WriteLine("y < 0");
+
+if (n == 1 || n == 4)
+    Console.WriteLine("x > 0");
+else
+    Console.WriteLine("x < 0");
